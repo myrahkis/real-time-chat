240d5bbc7a319f2fa038b838329329a6d5a74811
myrahkis <kruzinka@yandex.ru>
2025-01-26 19:17:13 +0300

    long pulling w/o styling

---
+++ src/components/LongPulling.vue
@@ -0,0 +1,52 @@
+<script setup>
+import axios from 'axios'
+import { nextTick, onMounted, ref } from 'vue'
+
+const messages = ref([])
+const newMes = ref('')
+
+onMounted(subscribe)
+
+async function subscribe() {
+  try {
+    const res = await axios.get('http://localhost:5000/get-message')
+
+    messages.value = [res.data, ...messages.value]
+
+    await subscribe()
+  } catch (err) {
+    setTimeout(() => subscribe(), 500)
+  }
+}
+
+async function sendMessage() {
+  if (newMes.value === '') return
+
+  try {
+    console.log('Отправка запроса...')
+    await axios.post('http://localhost:5000/new-message', {
+      message: newMes.value,
+      id: Date.now(),
+    })
+    console.log('Запрос отправлен успешно!')
+
+    newMes.value = ''
+  } catch (err) {
+    console.error('какая то муть')
+  }
+}
+</script>
+
+<template>
+  <div>
+    <input type="text" v-model.trim="newMes" />
+    <button @click="sendMessage">Send</button>
+  </div>
+  <div>
+    <ul>
+      <li v-for="mes in messages" :key="mes.id">
+        {{ mes.message }}
+      </li>
+    </ul>
+  </div>
+</template>
---
+++ src/server/longPulling.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import cors from 'cors'
+import events from 'events'
+
+const PORT = 5000
+
+// для регистрации и подписки на события
+const emitter = new events.EventEmitter()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+// польз отправ гет запрос, но ему не возвращается ответ,
+// подписываемся на событие и ждем
+app.get('/get-message', (req, res) => {
+  // всем польз у кот висит подлючение возвращается сообщение
+  emitter.once('newMessage', (mes) => res.json(mes))
+})
+
+// др участник чата отправляет сообщ
+app.post('/new-message', (req, res) => {
+  console.log(req.body)
+  const message = req.body
+
+  emitter.emit('newMessage', message)
+
+  res.status(200)
+})
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
