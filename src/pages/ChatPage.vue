171d1565f70fad6dcd582230d49ef8f9f4ebb57d
myrahkis <kruzinka@yandex.ru>
2025-01-27 21:46:20 +0300

    message input somewhat done

--- src/pages/ChatPage.vue
+++ src/pages/ChatPage.vue
@@ -12,6 +12,20 @@ onMounted(() => {
 </script>
 
 <template>
-  <MessageInput />
-  <ChatDisplay />
+  <div class="page-containter">
+    <MessageInput />
+    <ChatDisplay />
+  </div>
 </template>
+
+<style scoped>
+.page-containter {
+  display: flex;
+  flex-direction: column-reverse;
+  justify-content: space-between;
+  height: 100vh;
+  width: 55vw;
+  border: 6px solid var(--green-color);
+  border-bottom: none;
+}
+</style>
